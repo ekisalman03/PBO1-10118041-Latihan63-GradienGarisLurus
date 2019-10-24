92991e74f9ca0de8151ba91e4d173c0684679592
ekisalman03 <55838966+ekisalman03@users.noreply.github.com>
2019-10-24 21:06:55 +0700

    Add files via upload

---
+++ PBO1-10118041-Latihan63-GradienGarisLurus/src/pbo/pkg10118041/latihan63/gradiengarislurus/GarisLurus.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.pkg10118041.latihan63.gradiengarislurus;
+
+/**
+ *
+ * @author riski
+ */
+public interface GarisLurus {
+    
+    public int hitungGradien();
+    
+}
